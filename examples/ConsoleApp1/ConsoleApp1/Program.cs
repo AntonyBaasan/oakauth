5fa1e921197e4c41f25abaca68807cef1c85c4d2
Antony <antony.baasan@gmail.com>
2020-03-28 15:36:50 -0400

    add example projects

---
+++ examples/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(
+                new ClientCredentialsTokenRequest
+                {
+                    Address = disco.TokenEndpoint,
+                    ClientId = "client",
+                    ClientSecret = "secret",
+                    Scope = "api1"
+                });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+
+            await MakeRequestAsync(tokenResponse.AccessToken);
+
+            Console.ReadKey(); 
+        }
+
+        static async System.Threading.Tasks.Task MakeRequestAsync(string accessToken)
+        {
+            var client = new HttpClient();
+            client.SetBearerToken(accessToken);
+
+            var response = await client.GetAsync("https://localhost:6001/weatherforecast");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+        }
+    }
+}
---
+++ src/Web/Demo/Config.cs
@@ -0,0 +1,27 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Web.Demo
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiResource> Api =>
+            new List<ApiResource>
+            {
+                new ApiResource("api2", "Test Api")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+                    AllowedScopes = { "api2" }
+                }
+            };
+
+    }
+}
