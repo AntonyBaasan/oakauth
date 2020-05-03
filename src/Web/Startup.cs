f60a9fedbca13b6549a93ba72c8b6a22e19dbb18
Antony Baasandorj <antony.baasan@gmail.com>
2020-05-02 23:39:11 -0400

    enable migration

--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -60,7 +60,7 @@ namespace Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //InitializeDatabase(app);
+            InitializeDatabase(app);
 
             if (env.IsDevelopment())
             {
