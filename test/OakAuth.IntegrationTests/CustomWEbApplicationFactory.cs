b8acc22b6adc0bca2b6022ff06e8737ea33fe8c5
Antony <antony.baasan@gmail.com>
2020-05-25 23:35:56 -0400

    fix sit

--- test/OakAuth.IntegrationTests/CustomWEbApplicationFactory.cs
+++ test/OakAuth.IntegrationTests/CustomWEbApplicationFactory.cs
@@ -9,7 +9,13 @@ namespace OakAuth.IntegrationTests
         protected override IWebHostBuilder CreateWebHostBuilder()
         {
             return WebHost.CreateDefaultBuilder(null)
-                      .UseStartup<TestStartup>();
+                    .UseStartup<TestStartup>();
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseContentRoot(".");
+            base.ConfigureWebHost(builder);
         }
 
     }
